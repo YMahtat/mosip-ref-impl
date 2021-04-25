0d61cd0a4842894f0c3dc478f9cf6a092cebf362
Youssef MAHTAT <youssef.mahtat@gmail.com>
2021-04-25 21:01:35 +0000

    Update environment.prod.ts

--- admin-ui/src/environments/environment.prod.ts
+++ admin-ui/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-    production: true
+    production: true,
+    realBaseBasicUrl: 'https://mosipsbox-dev2.rnpdev.shop',
+    baseBasicUrl: 'https://mosipsbox-dev2.rnpdev.shop/',
+    baseUrl: 'https://mosipsbox-dev2.rnpdev.shop/v1/',
+    realBaseUrl: 'https://mosipsbox-dev2.rnpdev.shop/v1/'
 };
